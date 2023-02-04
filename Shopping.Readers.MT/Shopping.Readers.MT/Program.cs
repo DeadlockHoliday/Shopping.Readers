f5994c8a13e6237958915a82fc09552d7e43482c
Alex D.Lock Holiday <DeadlockHoliday@mail.ru>
2023-02-04 13:01:11 +0300

    Changed input url

--- Shopping.Readers.MT/Shopping.Readers.MT/Program.cs
+++ Shopping.Readers.MT/Shopping.Readers.MT/Program.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Shopping.Readers.MT.Tests")]
 
 var fileNowTimeStamp = DateTime.UtcNow.ToFileTimeUtc();
-var input = "https://gist.githubusercontent.com/DeadlockHoliday/8f4cf41fa7f2db97df40d0eff9d607ef/raw/b253538fcd5a215659450b331ac9f506abd229fe/gistfile1.txt";
+var input = "https://raw.githubusercontent.com/DeadlockHoliday/Shopping.Samples/main/Shopping.Samples.Readers.Input/MT.html";
 var output = $"products.mt.{fileNowTimeStamp}.csv";
 
 var inputHtml = GetHtml(input);

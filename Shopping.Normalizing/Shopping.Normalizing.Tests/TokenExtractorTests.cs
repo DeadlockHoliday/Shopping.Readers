40dfc3571d4826bd3ac6fe0680541ada7c46a54d
Alex D.Lock Holiday <DeadlockHoliday@mail.ru>
2023-02-15 10:07:56 +0300

    Extracted low level code into TokenExtractor.cs

---
+++ Shopping.Normalizing/Shopping.Normalizing.Tests/TokenExtractorTests.cs
@@ -0,0 +1,21 @@
+﻿using static Shopping.Normalizing.TokenExtractor;
+
+namespace Shopping.Normalizing.Tests;
+
+internal class TokenExtractorTests
+{
+    [TestCase("Стакан 200 мл")]
+    [TestCase("Стакан 200мл")]
+    [TestCase("Стакан 200 мл.")]
+    [TestCase("Стакан 200мл.")]
+    public void ExtractTokens_ShouldReturn_CorrectResult(string line)
+    {
+        var expected = new MeasureToken[]
+        {
+            new(200, "мл")
+        };
+
+        var actual = ExtractTokens(line);
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+}

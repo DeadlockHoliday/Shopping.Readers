36ad48e073a0567a0a83e5f311f0fd85732fb25a
DeadlockHoliday <deadlockholiday@mail.ru>
2023-03-03 15:21:54 +0300

    Introduced a repository
    relates #30

--- Shopping.Common/Data/Product.cs
+++ Shopping.Common/Data/Product.cs
@@ -10,13 +10,4 @@ public readonly record struct Product
     public required string Info { get; init; }
     public required string Category { get; init; }
     public readonly JsonObject Features { get; init; }
-
-    [SetsRequiredMembers]
-    public Product(string info, string category)
-    {
-        Info = info;
-        Category = category;
-        ProcessorVersion = string.Empty;
-        Features = new JsonObject();
-    }
 }
---
+++ Shopping.Data/Shopping.Data/SupplyPositionRepository.cs
@@ -0,0 +1,71 @@
+﻿using LiteDB;
+using Shopping.Common.Data.Supply;
+using Shopping.Common.Modules;
+
+namespace Shopping.Data;
+
+public interface ISupplyPositionRepository
+{
+    void Add(SupplyPosition position);
+    void Update(SupplyPosition position);
+    void Remove(SupplyPosition position);
+    SupplyPosition Get(string jsonPath);
+    SupplyPosition[] Load(string jsonPath);
+}
+
+public class SupplyPositionRepository : ISupplyPositionRepository
+{
+    private readonly string connectionString;
+
+    public SupplyPositionRepository(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public void Add(SupplyPosition position)
+    {
+        using var db = new LiteDatabase(connectionString);
+        var collection = db.GetCollection<SupplyPosition>();
+        collection.Insert(position);
+    }
+
+    public SupplyPosition Get(string jsonPath)
+    {
+        if (!string.IsNullOrWhiteSpace(jsonPath))
+        {
+            throw new NotImplementedException("jsonPath is under development");
+        }
+
+        using var db = new LiteDatabase(connectionString);
+        var collection = db.GetCollection<SupplyPosition>();
+        return collection.Query().First();
+    }
+
+    public SupplyPosition[] Load(string jsonPath)
+    {
+        if (!string.IsNullOrWhiteSpace(jsonPath))
+        {
+            throw new NotImplementedException("jsonPath is under development");
+        }
+
+        using var db = new LiteDatabase(connectionString);
+        var collection = db.GetCollection<SupplyPosition>();
+        return collection.Query().ToArray();
+    }
+
+    public void Remove(SupplyPosition position)
+    {
+        using var db = new LiteDatabase(connectionString);
+        var collection = db.GetCollection<SupplyPosition>();
+        // collection.Delete()
+        throw new NotImplementedException("No Id Provided.");
+    }
+
+    public void Update(SupplyPosition position)
+    {
+        using var db = new LiteDatabase(connectionString);
+        var collection = db.GetCollection<SupplyPosition>();
+        // collection.Update()
+        throw new NotImplementedException("No Id Provided.");
+    }
+}
